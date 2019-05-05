f2738a9ab86fa5cf04cf184d422da6b49015c4d3
Mark C. Mu <muchao1303@gmail.com>
2019-05-05 20:34:58 +0800

    Generate dao model and mapper files

--- src/main/java/com/mark/vote/VoteApplication.java
+++ src/main/java/com/mark/vote/VoteApplication.java
@@ -1,13 +1,38 @@
 package com.mark.vote;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class VoteApplication {
+public class VoteApplication implements ApplicationRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoteApplication.class, args);
 	}
 
+	private void generateArtifacts() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(this.getClass().getResourceAsStream("/generatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(true);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		generateArtifacts();
+	}
 }
