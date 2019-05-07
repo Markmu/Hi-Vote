108c27efd14ef4f045ef079d16c5c8d9beded61b
Mark C. Mu <muchao1303@gmail.com>
2019-05-07 23:34:12 +0800

    Open mybatis mapper scan

--- src/main/java/com/mark/vote/VoteApplication.java
+++ src/main/java/com/mark/vote/VoteApplication.java
@@ -4,18 +4,18 @@ import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
+@MapperScan("com.mark.vote.dao")
 public class VoteApplication implements ApplicationRunner{
 
 	public static void main(String[] args) {
@@ -33,6 +33,6 @@ public class VoteApplication implements ApplicationRunner{
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		generateArtifacts();
+//		generateArtifacts();
 	}
 }
